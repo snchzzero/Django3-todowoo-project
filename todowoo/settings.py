4e3b33324af22505ec2c7a06f8c2c181763eac3f
snchzzero <aaevstafev@gmail.com>
2022-05-27 18:08:04 +0300

    Frontend nick

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 LOGIN_URL = '/login'  # перенаправляет всех новых незарегистрированных пользователей на страницу регистрации
 
